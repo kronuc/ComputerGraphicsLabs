7f73f83e0c5b19b5e18937284e9d88fb8d69c191
kronuc <denerator2@gmail.com>
2022-02-21 20:48:19 +0200

    Add project files.

---
+++ ComputerGraphicsLabs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerGraphicsLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
